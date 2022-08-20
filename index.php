63756bc339aa6657aa114b2bd24e9de90aaad35e
Pratik2997 <pratik.paudel456@gmail.com>
2022-08-20 09:59:45 +0545

    second commit

--- index.php
+++ index.php
@@ -14,14 +14,25 @@ class ExportSchedulePlugin
 
         add_action('init', array($this, 'export_schedule_post_type'));
         add_shortcode('es_table', array($this, 'export_schedule_table'));
-        add_action('wp_enqueue_scripts', array($this,'additional_styles'));
+        add_action('wp_enqueue_scripts', array($this, 'frontend_styles'));
+        add_action('admin_enqueue_scripts', array($this, 'backend_styles'));
+        add_action('add_meta_boxes', array($this, 'es_meta_box'));
+        add_action('save_post', array($this, 'es_save_meta_box_data'));
     }
-    function additional_styles(){
-        wp_register_style('custom-css', plugin_dir_url(__FILE__) . "/style.css");
-        wp_register_script( 'bootstrap-bundle',"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js");
+    function frontend_styles()
+    {
+        wp_register_style('frontend-css', plugin_dir_url(__FILE__) . "/css/frontend.css");
 
-        wp_enqueue_style('custom-css');
-        wp_enqueue_script( 'bootstrap-bundle' );
+        wp_register_script('bootstrap-bundle', "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js");
+
+        wp_enqueue_style('frontend-css');
+        wp_enqueue_script('bootstrap-bundle');
+    }
+    function backend_styles()
+    {
+        wp_register_style('backend-css', plugin_dir_url(__FILE__) . "/css/backend.css");
+
+        wp_enqueue_style('backend-css');
     }
     function export_schedule_post_type()
 
@@ -48,9 +59,115 @@ class ExportSchedulePlugin
     {
         ob_start();
         require("template.php");
-       load_template("template.php");
-       return ob_get_clean();
+        load_template("template.php");
+        return ob_get_clean();
+    }
+    function es_meta_box()
+    {
+        //this will add the metabox for the member post type
+        $screens = array('export-schedule');
+
+        foreach ($screens as $screen) {
+
+            add_meta_box(
+                'es_tableid',
+                __('ES Table Details', 'es_textdomain'),
+                array($this, 'es_table_meta_box_callback'),
+                $screen
+            );
+        }
+    }
+
+    /**
+     * Prints the box content.
+     *
+     * @param WP_Post $post The object for the current post/page.
+     */
+    function es_table_meta_box_callback($post)
+    {
+
+        // Add a nonce field so we can check for it later.
+        wp_nonce_field('es_save_meta_box_data', 'es_meta_box_nonce');
+
+        /*
+     * Use get_post_meta() to retrieve an existing value
+     * from the database and use the value for the form.
+     */
+        $value = get_post_meta($post->ID, '_my_meta_value_key', true);
+?>
+
+        <div class="date-fields">
+            <label for="order_period_date_first">Order Period (First Date)</label>
+            <input type="date" name="order_period_date_first" id="order_period_date_first" value="<?php echo esc_attr($value[0]) ?>">
+        </div>
+
+        <div class="date-fields">
+            <label for="order_period_date_second">Order Period (Second Date)</label>
+            <input type="date" name="order_period_date_second" id="order_period_date_second" value="<?php echo esc_attr($value[1]) ?>">
+        </div>
+
+        <div class="date-fields">
+            <label for="export_date">Export Date</label>
+            <input type="date" name="export_date" id="export_date" value="<?php echo esc_attr($value[2]) ?>">
+        </div>
+
+        <div class="date-fields">
+            <label for="receive_date_first">Receive Date (First)</label>
+            <input type="date" name="receive_date_first" id="receive_date_first" value="<?php echo esc_attr($value[3]) ?>">
+        </div>
+
+        <div class="date-fields">
+            <label for="receive_date_second">Receive Date (Second)</label>
+            <input type="date" name="receive_date_second" id="receive_date_second" value="<?php echo esc_attr($value[4]) ?>">
+        </div>
+
+
+<?php
+    }
+
+    /**
+     * When the post is saved, saves our custom data.
+     *
+     * @param int $post_id The ID of the post being saved.
+     */
+    function es_save_meta_box_data($post_id)
+    {
+
+
+        if (!isset($_POST['es_meta_box_nonce'])) {
+            return;
+        }
+
+        if (!wp_verify_nonce($_POST['es_meta_box_nonce'], 'es_save_meta_box_data')) {
+            return;
+        }
+
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+
+        // Check the user's permissions.
+        if (isset($_POST['post_type']) && 'page' == $_POST['post_type']) {
+
+            if (!current_user_can('edit_page', $post_id)) {
+                return;
+            }
+        } else {
+
+            if (!current_user_can('edit_post', $post_id)) {
+                return;
+            }
+        }
+
+        $my_data = array($_POST['order_period_date_first'], $_POST['order_period_date_second'], $_POST['export_date'], $_POST['receive_date_first'], $_POST['receive_date_second']);
+
+        if (!isset($my_data)) {
+            return;
+        }
         
+        $my_data = array_map('sanitize_text_field', $my_data);
+
+        update_post_meta($post_id, '_my_meta_value_key', $my_data);
     }
 }
 
--- template.php
+++ template.php
@@ -11,45 +11,53 @@
             <?php
             $args = array(
                 'post_type' => 'export-schedule',
-                'posts_per_page' => 5
+                'posts_per_page' => 1
             );
             $export_schedules = new WP_Query($args);
             if ($export_schedules->have_posts()) : while ($export_schedules->have_posts()) : $export_schedules->the_post();
+                    $value = get_post_meta(get_the_ID(), '_my_meta_value_key', true);
                     $today = date('Y-m-d');
-                    $today = date('Y-m-d', strtotime($today));
-                    $firstOrderDate = date('Y-m-d', strtotime(get_field("order_period_date_first")));
-                    $lastOrderDate = date('Y-m-d', strtotime(get_field("order_period_date_second")));
-                    $orderPeriodMonthFirst = date('m', strtotime(get_field("order_period_date_first")));
-                    $orderPeriodMonthSecond = date('m', strtotime(get_field("order_period_date_second")));
-                    $receiveDateMonthFirst = date('m', strtotime(get_field("receive_date_first")));
-                    $receiveDateMonthSecond = date('m', strtotime(get_field("receive_date_second")));
+                    if (isset($value[0]) && isset($value[1]) && isset($value[2]) && isset($value[3]) && isset($value[4])) {
+                        $firstOrderDate = $value[0];
+                        $lastOrderDate = $value[1];
+                        $orderPeriodMonthFirst = date('m', strtotime($value[0]));
+                        $orderPeriodMonthSecond = date('m', strtotime($value[1]));
+                        $receiveDateMonthFirst = date('m', strtotime($value[3]));
+                        $receiveDateMonthSecond = date('m', strtotime($value[4]));
+                    }
             ?>
 
                     <tr>
                         <td>
                             <?php
-                            if ($orderPeriodMonthFirst == $orderPeriodMonthSecond) {
-                                echo date('d', strtotime(get_field("order_period_date_first"))) . " - " . date('d', strtotime(get_field("order_period_date_second"))) . " " . date('F', strtotime(get_field("order_period_date_first"))) . " " . date('Y', strtotime(get_field("order_period_date_first")));
-                            } else {
-                                echo date('d', strtotime(get_field("order_period_date_first"))) . date('F', strtotime(get_field("order_period_date_first"))) . " - " . date('d', strtotime(get_field("order_period_date_second"))) . date('F', strtotime(get_field("order_period_date_second"))) . " " . date('Y', strtotime(get_field("order_period_date_first"))) . " ";
+                            if (isset($value[0]) && isset($value[1])) {
+                                if ($orderPeriodMonthFirst == $orderPeriodMonthSecond) {
+                                    echo date('d', strtotime($value[0])) . " - " . date('d', strtotime($value[1])) . " " . date('F', strtotime($value[0])) . " " . date('Y', strtotime($value[0]));
+                                } else {
+                                    echo date('d', strtotime($value[0])) . " ". date('F', strtotime($value[0])) . " - " . date('d', strtotime($value[1])). " " . date('F', strtotime($value[1])) . " " . date('Y', strtotime($value[0])) . " ";
+                                }
                             }
-                            if (($today >= $firstOrderDate) && ($today <= $lastOrderDate)){
+
+                            if (($today >= $firstOrderDate) && ($today <= $lastOrderDate)) {
                                 echo ' <svg class = "custom" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                               </svg>' . " " . "Current Period";
-                            }else{
-                                echo "";  
+                            } else {
+                                echo "";
                             }
 
                             ?>
                         </td>
-                        <td><?php echo get_field("export_date") ?></td>
+                        <td><?php if (isset($value[2])) : echo $value[2];
+                            endif; ?></td>
                         <td>
                             <?php
-                            if ($receiveDateMonthFirst == $receiveDateMonthSecond) {
-                                echo date('d', strtotime(get_field("receive_date_first"))) . " - " . date('d', strtotime(get_field("receive_date_second"))) . " " . date('F', strtotime(get_field("receive_date_first"))) . " " . date('Y', strtotime(get_field("receive_date_first")));
-                            } else {
-                                echo date('d', strtotime(get_field("receive_date_first"))) . " " . date('F', strtotime(get_field("receive_date_first"))) . " - " . date('d', strtotime(get_field("receive_date_second"))) . " " . date('F', strtotime(get_field("receive_date_second"))) . " " . date('Y', strtotime(get_field("receive_date_first")));
+                            if (isset($value[3]) && isset($value[4])) {
+                                if ($receiveDateMonthFirst == $receiveDateMonthSecond) {
+                                    echo date('d', strtotime($value[3])) . " - " . date('d', strtotime($value[4])) . " " . date('F', strtotime($value[3])) . " " . date('Y', strtotime($value[3]));
+                                } else {
+                                    echo date('d', strtotime($value[3])) . " " . date('F', strtotime($value[3])) . " - " . date('d', strtotime($value[4])) . " " . date('F', strtotime($value[4])) . " " . date('Y', strtotime($value[3]));
+                                }
                             }
                             ?>
                         </td>
